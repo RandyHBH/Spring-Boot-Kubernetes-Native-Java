d9c861c4162a05c83a54cae041561fe4775f62a8
Randy Hector Bartumeu Huergo <randy.bartumeu@mytaxi.com>
2021-06-19 14:19:46 +0200

    Add Availability change

--- src/main/kotlin/com/example/customers/CustomersApplication.kt
+++ src/main/kotlin/com/example/customers/CustomersApplication.kt
@@ -3,11 +3,15 @@ package com.example.customers
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.availability.AvailabilityChangeEvent
+import org.springframework.boot.availability.LivenessState
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 
@@ -25,6 +29,12 @@ class CustomerRestController(private val repo: CustomerRepository) {
 	  fun get() = this.repo.findAll()
 }
 
+@RestController
+class AvailabilityController(private val ac: ApplicationContext) {
+
+	  @PostMapping("/down")
+	  fun down() = AvailabilityChangeEvent.publish(this.ac, LivenessState.BROKEN)
+}
 
 @Component
 class SampleDataInitializer(private val repo: CustomerRepository) : ApplicationRunner {
